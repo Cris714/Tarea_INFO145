e91136f5262e2e06f00afd0cea150fdf0a4ef222
Benjamín <benrrixworks@gmail.com>
2023-06-15 03:47:01 -0400

    Reverti los cambios en el primer escalera64LowestRoad.cpp ya que sin querer lo reemplace en el commit anterior xd

--- Escalera64/escalera64LowestRoad.cpp
+++ Escalera64/escalera64LowestRoad.cpp
@@ -6,76 +6,83 @@
 #include <cstdlib>
 
 using namespace std;
-#define INF (unsigned int) -1
 
 struct Escalon {
+    unsigned long number;
     unsigned long value;
     vector<unsigned long> road;
 };
 
-int escalera64(unsigned long* S, Escalon* E, int n, int k) {
-    
-    for (int j = 0; j < k; j++){
-        E[S[j]].value = 1;
-        E[S[j]].road.push_back(S[j] + 1);
-    }
-    unsigned long s;
-    for (int i = 0, j=0; i < n; i++, j=0) {
-        if (E[j].value != INF) {
-            while ((s=S[j++]) < E[j].value) {
+int escalera64(set<unsigned long> S, Escalon* E, set<unsigned long> D, int l) {
+
+    for (int i = 0; i < l; i++) {
+        if (D.find(i + 1) == D.end()) {
+            if (S.find(i + 1) != S.end()) {
+                E[i].value += 1;
+                E[i].road.push_back(i + 1);
+            }
+            for (int s : S) {
                 if (i - s >= 0 && E[i - s].value > 0) {
                     E[i].value += E[i - s].value;
                     vector<unsigned long> r = E[i - s].road;
                     r.push_back(i + 1);
                     if (E[i].road.empty() || r.size() < E[i].road.size() + 1) 
                         E[i].road = r;
+
                 }
-                else break;
             }
         }
     }
-    return E[n].value;
+    return E[l].value;
 }
 
 int main() {
-    
     srand(time(nullptr));
-    
+
     // Parametros de entrada
     int p = 2;
     int largoEscalera = 900;
-    
+
     // Creacion de vector con escalones
-    Escalon* E = new Escalon[largoEscalera]; //n
+    Escalon* E = new Escalon[largoEscalera];
     for (int i = 0; i < largoEscalera; i++) {
         Escalon e = Escalon();
+        e.number = i+1;
         e.value = 0;
         E[i]=e;
     }
-    
-    // Destruidos
-    int d = 0;
-    int r = 0;
-    for (int i=0;i<d;i++) while (E[(r=rand()%largoEscalera)].value == INF) E[r].value = INF;
-    
+
+    set<unsigned long> D = {};
+
     // Generacion de saltos respecto a P
-    int k = int(log2(largoEscalera) / log2(p));
-    unsigned long* S = new unsigned long [k];
-    for (int i = 0; i <= k; i++)
-        S.pow(p, i);
-    
-    /* Escribir en pantalla */
+    set<unsigned long> S;
+    for (int i = 0; i <= int(log2(largoEscalera) / log2(p)); i++) {
+        S.insert(pow(p, i));
+    }
+
     // Escribir la entrada
-    printf("E (largo): %d", largoEscalera);
+    if (largoEscalera < 101){
+        printf("E:[");
+        for (int i = 0; i < largoEscalera; i++) {
+            printf("%lu", E[i].number);
+            if (i < largoEscalera - 1) printf(", ");
+        }
+        printf("]");
+    }
+    else printf("E (largo): %d", largoEscalera);
+    printf("\nD: {");
+    for (unsigned long d : D) 
+        printf("%lu, ", d);
     printf("\b\b}\nS: {");
-    for (unsigned long s : S) printf("%lu, ", s);
-    
+    for (unsigned long s : S) 
+        printf("%lu, ", s);
+
     //Ejecutar
     printf("\b\b}\nSoluciones posibles: ");
     clock_t inicio = clock();
-    escalera64(S, E, largoEscalera, k);
+    escalera64(S, E, D, largoEscalera);
     clock_t final = clock();
-    
+
     // Escribir los Resultados
     printf("%lu",E[largoEscalera-1].value);
     if (largoEscalera < 101){
@@ -92,9 +99,9 @@ int main() {
         if (i < E[largoEscalera-1].road.size() - 1) printf(", ");
     }
     printf("]\n");
-    
+
     double tiempoEjecucion = double(final - inicio) / CLOCKS_PER_SEC;
     printf("Tiempo de ejecucion: %.6f\n", tiempoEjecucion);
-    
+
     return 0;
 }
